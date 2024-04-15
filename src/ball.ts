209cc01c1068fe8a5d7bbe73d05626b4b2f58c35
IwanTen <iwantraegerpayne@gmail.com>
2024-04-15 17:32:40 -0400

    add multiple instances of our ball class using config. TODO: Update balls physics depending on state

--- src/ball.ts
+++ src/ball.ts
@@ -1,12 +1,35 @@
 import * as THREE from 'three';
 
+enum State {
+  Idle,
+  Active,
+}
 export default class JugglingBall extends THREE.Mesh {
+  acceleration = new THREE.Vector3(0, 0, 0);
+  velocity = new THREE.Vector3(0, 0, 0);
+  state: State = State.Idle;
+  mass: number = 1;
   constructor(
     sphereGeometry: THREE.SphereGeometry,
-    material: THREE.MeshToonMaterial
+    material: THREE.MeshToonMaterial,
+    startPos: number[]
   ) {
     super(sphereGeometry, material);
 
-    console.log('This is a ball class that extends a three class');
+    this.position.set(startPos[0], startPos[1], startPos[2]);
+  }
+
+  updatePhysics(deltaTime: number, enableGravity: boolean = false) {
+    // Here we manually apply gravity to the balls acceleration.
+    enableGravity && this.acceleration.add(new THREE.Vector3(0, -9.8, 0));
+    this.velocity.add(this.acceleration.clone().multiplyScalar(deltaTime));
+    this.position.add(this.velocity.clone().multiplyScalar(deltaTime));
+    // console.log('Delta time:', deltaTime);
+    this.acceleration.set(0, 0, 0);
+  }
+
+  throwBall(force: THREE.Vector3) {
+    this.velocity.set(0, 0, 0);
+    this.acceleration.add(force.clone().divideScalar(this.mass));
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,26 @@
 import * as THREE from 'three';
+import JugglingBall from './ball';
 import * as dat from 'dat.gui';
-import Ball from './ball';
 
-//TODO: Extract the majority of following code into scene file.
 const canvas: HTMLCanvasElement | null = document.querySelector('canvas');
-
 const scene = new THREE.Scene();
 
-//* LIGHTING
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.06);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.08); //* LIGHTING
 const pointLight = new THREE.PointLight(0xff0000, 1, 100);
 pointLight.position.set(1, 2, 1);
 scene.add(ambientLight);
 scene.add(pointLight);
 
-//* CAMERA
-const camera = new THREE.PerspectiveCamera(
+const camera = new THREE.PerspectiveCamera( //* CAMERA
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-camera.position.z = 38;
+camera.position.z = 18;
 
-//* CREATE GEOMETRY (Sphere buffer geometry)
-const material = new THREE.MeshToonMaterial({ color: 0xff0000 });
+//* Create our base sphere and pass to our Ball class
+
 const SPHERE_FIDELITY = 16;
 const sphereGeometry = new THREE.SphereGeometry(
   1,
@@ -32,34 +28,44 @@ const sphereGeometry = new THREE.SphereGeometry(
   SPHERE_FIDELITY
 );
 
-const sphereMesh = new Ball(sphereGeometry, material);
-scene.add(sphereMesh);
+let deltaSeperation = 5;
+const JugglingBallConfig = [
+  {
+    startPos: [-deltaSeperation, 0, 0],
+    color: 0xff0000,
+  },
+  {
+    startPos: [deltaSeperation, 0, 0],
+    color: 0x00ff00,
+  },
+  // {
+  //   startPos: [0, deltaSeperation, 0],
+  //   color: 0x0000ff,
+  // },
+];
 
-//* MOUSE MOVE
-const mouse = new THREE.Vector2();
-const onMouseMove = (event: MouseEvent) => {
-  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-};
-window.addEventListener('mousemove', onMouseMove);
+const balls: JugglingBall[] = [];
 
-//* CLICK
-const raycaster = new THREE.Raycaster();
-const onClick = (event: MouseEvent) => {
-  raycaster.setFromCamera(mouse, camera);
-  const intersects = raycaster.intersectObjects(scene.children);
-  if (intersects.length > 0) {
-    const object = intersects[0].object;
-    if (object instanceof THREE.Mesh) {
-      console.log('clicked on object!', object);
-      //TODO: Add functionality to show target trajectory.
-    }
-  }
-};
-window.addEventListener('click', onClick);
+for (let ball of JugglingBallConfig) {
+  balls.push(
+    new JugglingBall(
+      sphereGeometry,
+      new THREE.MeshToonMaterial({ color: ball.color }),
+      ball.startPos
+    )
+  );
+}
 
-//* RENDERER + RESIZE
-const renderer = new THREE.WebGLRenderer({ canvas: canvas! });
+console.log(balls);
+
+balls.forEach((ball) => {
+  scene.add(ball);
+});
+
+// const Ball = new JugglingBall(sphereGeometry, material);
+// scene.add(Ball);
+
+const renderer = new THREE.WebGLRenderer({ canvas: canvas! }); //* RENDERER + RESIZE
 renderer.setSize(window.innerWidth, window.innerHeight);
 const resizeCanvas = () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -70,8 +76,8 @@ window.addEventListener('resize', resizeCanvas);
 
 let previousTime = 0;
 
-let enableGravity = true;
-let isPaused = true;
+let enableGravity = false;
+let isPaused = false;
 const gravity = new THREE.Vector3(0, -9.8, 0);
 const velocity = new THREE.Vector3(0, 0, 0);
 const acceleration = new THREE.Vector3(0, 0, 0);
@@ -79,45 +85,43 @@ const mass = 5;
 
 function animate(currentTime: number) {
   const deltaTime = (currentTime - previousTime) / 1000; // Calculate time difference in seconds
-  // console.log('Delta time: ', deltaTime); //! Debugging
   previousTime = currentTime;
-  // console.log('current time: ', currentTime); //! Debugging
 
   enableGravity && acceleration.add(gravity.clone());
 
   if (!isPaused) {
-    velocity.add(acceleration.clone().multiplyScalar(deltaTime));
-    sphereMesh.position.add(velocity.clone().multiplyScalar(deltaTime));
+    balls.forEach((ball) => {
+      ball.updatePhysics(deltaTime, enableGravity);
+    });
   }
 
-  requestAnimationFrame(animate);
   renderer.render(scene, camera);
   acceleration.set(0, 0, 0);
+  requestAnimationFrame(animate);
 }
 
 animate(0);
 
+function _applyForce(force: THREE.Vector3) {
+  velocity.set(0, 0, 0);
+  acceleration.add(force.clone().divideScalar(mass));
+}
+
+const _calculateKinematicPosition = (
+  initialPosition: THREE.Vector3,
+  initialVelocity: THREE.Vector3,
+  acceleration: THREE.Vector3,
+  time: number
+) => {
+  return initialPosition
+    .clone()
+    .add(initialVelocity.clone().multiplyScalar(time))
+    .add(acceleration.clone().multiplyScalar(0.5 * time * time));
+};
+
+//* GUI GUI GUI
+
 var gui = new dat.GUI({ name: 'My GUI' });
-gui
-  .add(
-    {
-      resetSpherePosition: () => {
-        sphereMesh.position.set(0, 0, 0);
-        velocity.set(0, 0, 0);
-      },
-    },
-    'resetSpherePosition'
-  )
-  .name('Reset Sphere Position');
-gui
-  .add({ pause: isPaused }, 'pause')
-  .name('Pause animation')
-  .onChange(() => {
-    isPaused = !isPaused;
-    if (isPaused) {
-      previousTime = 0;
-    }
-  });
 
 gui
   .add(
@@ -131,56 +135,27 @@ gui
   })
   .name('Apply gravity');
 
-gui
-  .add(
-    {
-      applyThrowForce: () => {
-        _applyForce(new THREE.Vector3(0, 1000, 0));
-        console.log('applying random force');
-      },
-    },
-    'applyThrowForce'
-  )
-  .name('Apply random force');
+// //TODO: Draw trajectory of target object.
 
-function _applyForce(force: THREE.Vector3) {
-  velocity.set(0, 0, 0);
-  acceleration.add(force.clone().divideScalar(mass));
-}
+// const lineMaterial = new THREE.LineBasicMaterial({ color: 0x00ff00 });
 
-const calculateKinematicPosition = (
-  initialPosition: THREE.Vector3,
-  initialVelocity: THREE.Vector3,
-  acceleration: THREE.Vector3,
-  time: number
-) => {
-  return initialPosition
-    .clone()
-    .add(initialVelocity.clone().multiplyScalar(time))
-    .add(acceleration.clone().multiplyScalar(0.5 * time * time));
-};
+// const projectedPoints = [];
+// const projectedTimeInSeconds = 2;
+// const lineResolution = 100;
 
-//TODO: Draw trajectory of target object.
+// for (let i = 0; i < lineResolution; i++) {
+//   const time = i * (projectedTimeInSeconds / lineResolution);
+//   const position = calculateKinematicPosition(
+//     Ball.position,
+//     new THREE.Vector3(10, 3, 0),
+//     new THREE.Vector3(6, -9.8, 0),
+//     time
+//   );
+//   projectedPoints.push(position);
+// }
 
-const lineMaterial = new THREE.LineBasicMaterial({ color: 0x00ff00 });
+// console.log('projected points: ', projectedPoints);
 
-const projectedPoints = [];
-const projectedTimeInSeconds = 2;
-const lineResolution = 100;
-
-for (let i = 0; i < lineResolution; i++) {
-  const time = i * (projectedTimeInSeconds / lineResolution);
-  const position = calculateKinematicPosition(
-    sphereMesh.position,
-    new THREE.Vector3(10, 3, 0),
-    new THREE.Vector3(6, -9.8, 0),
-    time
-  );
-  projectedPoints.push(position);
-}
-
-console.log('projected points: ', projectedPoints);
-
-const lineGeometry = new THREE.BufferGeometry().setFromPoints(projectedPoints);
-const line = new THREE.Line(lineGeometry, lineMaterial);
-scene.add(line);
+// const lineGeometry = new THREE.BufferGeometry().setFromPoints(projectedPoints);
+// const line = new THREE.Line(lineGeometry, lineMaterial);
+// scene.add(line);
